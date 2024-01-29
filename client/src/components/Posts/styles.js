f44195bbe50c4835e0dc9151f5d0df5d61314e5f
Keerthy Baby <keerthybaby025@gmail.com>
2024-01-29 21:44:36 +0530

    Add MongoDB and Redux to project initialization

---
+++ client/src/components/Posts/styles.js
@@ -0,0 +1,16 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+export default makeStyles((theme)=>({
+mainCointainer:{
+  display:'flex',
+  alignItems:'center',
+
+},
+snMargin:{
+  margin:theme.spacing(1),
+
+},
+actionDiv:{
+  textAlign:'center',
+}
+}));
---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+//import { createStore, applyMiddleware, compose } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { thunk } from "redux-thunk";
+
+import reducers from "./reducers";
+import App from "./App";
+
+//const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
