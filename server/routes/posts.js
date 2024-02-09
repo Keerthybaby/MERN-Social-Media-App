7236f7cd00628abf7a2e7715711e71f356dda445
Keerthy Baby <keerthybaby025@gmail.com>
2024-02-09 17:05:09 +0530

    Add Search

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from "express";
+//import { getPostsBySearch } from "../../client/src/actions/posts.js";
 
-import { getPosts, createPost,updatePost,deletePost,likePost } from "../controllers/posts.js";
+import { getPostsBySearch, getPosts, createPost,updatePost,deletePost,likePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
+router.get('/search',getPostsBySearch);
 router.get("/", getPosts);
 router.post("/", createPost);
 router.patch('/:id',updatePost);
